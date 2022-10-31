bd19e164b2c62f328ac0789012533c050177174b
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-10-30 20:02:35 -0500

    Create comments.swift

---
+++ comments.swift
@@ -0,0 +1,13 @@
+//
+//  comments.swift
+//  Basics of Swift Programming
+//
+//  Created by Jayce Sagvold on 10/30/22.
+//
+
+import Foundation
+
+// These are commments
+// You can use these anywhere in your code
+// You can use this to show people what that code does or just a note to your self
+
