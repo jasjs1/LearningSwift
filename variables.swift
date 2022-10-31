25cb01c45a5f59546efa37c95d0a19b17d98f1db
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-10-30 19:55:53 -0500

    Create variables.swift

---
+++ variables.swift
@@ -0,0 +1,20 @@
+//
+//  varibles.swift
+//  Basics of Swift Programming
+//
+//  Created by Jayce Sagvold on 10/30/22.
+//
+
+import SwiftUI
+
+var VaribleOne = 1
+
+var VaribleONe = 5
+
+let VaribleTwo = 59
+
+let varbileThree = 11
+
+var Name = "Jayce" // Feel free to use what ever you want here
+
+var Name = "Your name"
