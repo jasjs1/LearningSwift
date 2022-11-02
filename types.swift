f0e68c79dc6870c950a47aa4da4c5cbe2c86047a
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-01 19:55:48 -0500

    Create types.swift

---
+++ types.swift
@@ -0,0 +1,32 @@
+//
+//  types.swift
+//  Basics of Swift Programming
+//
+//  Created by Jayce Sagvold on 11/1/22.
+//
+
+import Foundation // DONT DO swiftUI
+import UIKit // This holds images, butons, anytthing UI related
+// Types
+
+var x = 3 // this is a interger
+var name = "Joe" // this is a string
+
+var lastName: String = 123
+let age: Int = 22
+let price: Double = 1.99
+var height: Float = 200.1234 // cm
+
+
+class Person {
+    
+}
+
+var john: Person = Person()
+// Or also can write as:
+// var john = Person()
+
+var button1: UIButton = UIButton()
+
+var isComplete: Bool = false
+var isComplete = true
