d5a9fcd5bbea4707c7def0e86f7bd9b8eb05d33d
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-01 20:08:47 -0500

    Create functionsAndPeramder.swift
    May be buggy

---
+++ functionsAndPeramder.swift
@@ -0,0 +1,36 @@
+//
+//  functionsAndParameters.swift
+//  Basics of Swift Programming
+//
+//  Created by Jayce Sagvold on 11/1/22.
+//
+
+import Foundation
+
+// Functions and Paramaders
+
+func mFirstFunction() {
+    
+}
+
+func addTwoNumbers(a: Int, b: Int) -> Int { // after -> will change depending on what you are doing
+    return a + b
+}
+
+
+func squareNumber(number: Int) -> Int {
+    return number * number
+}
+let squaredSum = squareNumber(a: x, b: y)
+
+let x = 3
+var y = 123141
+let sum = addTwoNumbers(a: x, b: y)
+
+func isEven(numberL Int) -> Bool {
+    if number % 2 == 0 {
+        return true
+    }
+    return false
+}
+var number = number
